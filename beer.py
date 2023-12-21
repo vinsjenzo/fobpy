40004f037193a600ef3c5425407dd0a32c6aa7f5
vinsjenzo <vjfvos@gmail.com>
2023-12-21 16:02:58 +0100

    Create pylint.yml (#3)
    * Create pylint.yml
    
    * fix beer.py lint issues
    
    * added class docstring
    
    * fix some linting errors
    
    * fix string thing
    
    * fixed some linting tings yaknow
    
    * fixed more linting yaknow
    
    * fixed even more linting tings
    
    * je weet toch
    
    * sike
    
    * more linting pls
    
    * jwz
    
    * yalla yalla
    
    * maybe i'm finally done now
    
    * siktirgit kaag
    
    * kaka
    
    * ok i give up
    
    * fixed redefined_outer_name lint issues?
    
    * finally done ?
    
    * finally done.

--- beer.py
+++ beer.py
@@ -1,9 +1,15 @@
-class beer(object):
-     def __init__(self, name, style, abv: float, info):
-         self.name = name
-         self.style = style
-         self.abv = abv
-         self.info = info
+"""Module containting beer class definition"""
+class Beer:
+    """Class representing a beer"""
+    def __init__(self, name, style, abv: float, info):
+        self.name = name
+        self.style = style
+        self.abv = abv
+        self.info = info
 
-     def __lt__(self, other):
-         return self.abv < other.abv
+    def __lt__(self, other):
+        return self.abv < other.abv
+
+    def get_name(self):
+        """Get the name of a beer."""
+        return self.name
--- flight_of_beer_generator.py
+++ flight_of_beer_generator.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+"""Module for printing a styled document with info for selected beers."""
 import csv
 import os
 import urllib.request
-from docxtpl import DocxTemplate
 import datetime
-from beer import beer
+from docxtpl import DocxTemplate
+from beer import Beer
 
 FAIL = '\033[91m'
 ENDC = '\033[0m'
@@ -12,136 +13,148 @@ OKGREEN = '\033[92m'
 
 print('Welcome!\n')
 
-file_paths = ['currentdraft.txt', 'template.docx', 'archive.txt']
+BASE_PATH = "https://raw.githubusercontent.com/vinsjenzo/fobpy/main"
+file_names = ['currentdraft.txt', 'template.docx', 'archive.txt']
 
-for file_path in file_paths:
-    if not os.path.exists(file_path):
-        urllib.request.urlretrieve(f"https://raw.githubusercontent.com/vinsjenzo/fobpy/main/{file_path}", file_path)
+for file_name in file_names:
+    if not os.path.exists(file_name):
+        urllib.request.urlretrieve(f"{BASE_PATH}/{file_name}", file_name)
 
 def parse_csv_file(filename):
-    beerList = []
-    with open(filename) as csvfile:
+    """Function parsing a beerlist txt file, returning a list of beers, name;style;abv;info."""
+    _beer_list = []
+    with open(filename, encoding="UTF-8") as csvfile:
         try:
             csv_reader = csv.reader(csvfile, delimiter=';')
             for row in csv_reader:
-                beerName = row[0]
-                beerStyle = row[1]
+                name = row[0]
+                style = row[1]
                 abv = float(row[2])
                 info = row[3]
-                beerList.append(beer(beerName, beerStyle, abv, info))
+                _beer_list.append(Beer(name, style, abv, info))
             # print(f'Parsed {len(beerList)} beers.\n')
-            return beerList
-        except:
+            return _beer_list
+        except FileNotFoundError:
+            print(f"File not found: {filename}\n")
             return None
-        
-def print_beers(beerList):
-    for i in range(len(beerList)):
-        print(f'{i+1}. {beerList[i].name}')
 
-def create_new_fob_doc(chosenBeersList):
+def print_beers(_beer_list):
+    """Function printing a list of beers."""
+    for index, beer in enumerate(_beer_list):
+        print(f'{index+1}. {beer.name}')
+
+def create_new_fob_doc(_chosen_beers_list):
+    """Function creating a styled word document from a list of beers."""
     template = DocxTemplate('template.docx')
     now = datetime.datetime.now()
     day = now.strftime('%d')
     month = now.strftime('%m')
     year = now.strftime('%y')
-    
+
     context = {
         'day': day,
         'month': month,
-        'chosenBeersList': chosenBeersList
+        'chosenBeersList': _chosen_beers_list
     }
-    print(chosenBeersList[0].style)
+    print(_chosen_beers_list[0].style)
 
     template.render(context, autoescape=True)
     try:
         template.save(f'Flight of Beer {day}-{month}-{year}.docx')
     except PermissionError:
-        print(f"\n{FAIL}Could not save the generated document, please close the opened word document!{ENDC} \n")
+        print(f"\n{FAIL}Couldnt save the generated document"
+              f", please close the opened word document!{ENDC} \n")
         return False
 
     print(f"{OKGREEN}Flight of beer document succesfully generated!{ENDC}")
     return True
 
-def write_list_to_file(currentList, filename):
-    with open(filename, 'w', newline='') as csvfile:
+def write_list_to_file(current_list, filename):
+    """Fuction writing a beerlist to a .txt file."""
+    with open(filename, 'w', encoding='UTF-8', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=';')
-        for beer in currentList:
+        for beer in current_list:
             csvwriter.writerow([beer.name, beer.style, beer.abv, beer.info])
 
 def remove_beer_from_current_draft_list():
-    currentList = parse_csv_file('currentdraft.txt')
-    print_beers(currentList)
+    """Function removing a beer from a list."""
+    current_list = parse_csv_file('currentdraft.txt')
+    print_beers(current_list)
     response = input("Select beer to remove.\tChoose q to cancel.\n")
-    if(response == 'q'):
+    if response == 'q':
         return
-    try: 
+    try:
         index_to_delete = int(response)-1
     except ValueError:
         print(f'{FAIL}Sorry, did not understand that!{ENDC}')
         return
-    if(index_to_delete < 0 or index_to_delete >= len(currentList)):
+    if(index_to_delete < 0 or index_to_delete >= len(current_list)):
         print(f"{FAIL}Index out of range!{ENDC}")
         return
-    currentList.pop(index_to_delete)
-    currentList.sort()
-    write_list_to_file(currentList,'currentdraft.txt')
+    current_list.pop(index_to_delete)
+    current_list.sort()
+    write_list_to_file(current_list,'currentdraft.txt')
 
-    
+
 def add_beer_to_current_draft_list():
-    currentList = parse_csv_file('currentdraft.txt')
-    archiveList = parse_csv_file('archive.txt')
+    """Function adding a beer to curent draft list from archive.""" 
+    current_list = parse_csv_file('currentdraft.txt')
+    archive_list = parse_csv_file('archive.txt')
     print(30 * "-" , "CURRENT DRAFT" , 30 * "-")
-    print_beers(currentList)
+    print_beers(current_list)
     print(30 * "-" , "ARCHIVE" , 30 * "-")
-    print_beers(archiveList)
+    print_beers(archive_list)
 
     response = input("Select beer to add.\tChoose q to cancel.\n")
-    if(response == 'q'):
+    if response == 'q':
         return
-    try: 
-        indexToAdd = int(response)-1
+    try:
+        index_to_add = int(response)-1
     except ValueError:
         print(f'{FAIL}Sorry, did not understand that!{ENDC}')
         return
-    if(indexToAdd < 0 or indexToAdd >= len(archiveList)):
+    if(index_to_add < 0 or index_to_add >= len(archive_list)):
         print(f"{FAIL}Index out of range!{ENDC}")
         return
-    beerToAdd = archiveList[indexToAdd]
-    if any(beer.name == beerToAdd.name for beer in currentList):
+    beer_to_add = archive_list[index_to_add]
+    if any(beer.name == beer_to_add.name for beer in current_list):
         print(f"{FAIL}Beer already in draft list!{ENDC}")
         return
-    currentList.append(beerToAdd)
-    currentList.sort()
-    write_list_to_file(currentList, 'currentdraft.txt')
+    current_list.append(beer_to_add)
+    current_list.sort()
+    write_list_to_file(current_list, 'currentdraft.txt')
 
-def save_new_beer_in_archive(newBeer, filename):
-    beerList = parse_csv_file(filename)
-    if not any(beer.name == newBeer.name for beer in beerList):
-        beerList.append(newBeer)
-        beerList.sort()
-        write_list_to_file(beerList, filename)
+def save_new_beer_in_archive(_new_beer, filename):
+    """Function saving a new beer in archive file."""
+    _beer_list = parse_csv_file(filename)
+    if not any(beer.name == _new_beer.name for beer in _beer_list):
+        _beer_list.append(_new_beer)
+        _beer_list.sort()
+        write_list_to_file(_beer_list, filename)
         return True
     return False
 
 def get_new_beer_from_input():
+    """Function creating a new beer from input."""
     name = input("What is the name of the beer? ")
     style = input("What style is the beer? ")
     abv = input("What is the abv%? ")
     abv = abv.replace(',', '.')
     info = input("Please give a brief description of the beer: ")
-    
+
     try:
-        newBeer = beer(name, style, float(abv), info)
+        _new_beer = Beer(name, style, float(abv), info)
     except ValueError:
         print(f"{FAIL}Couldn't parse the input{ENDC}")
         return None
-    return newBeer
+    return _new_beer
 
 
-def print_menu():       ## Your menu design here
-    currentList = parse_csv_file('currentdraft.txt')
+def print_menu():
+    """Function printing the program menu."""
+    current_list = parse_csv_file('currentdraft.txt')
     print(25 * "-" , "CURRENT DRAFTS" , 25 * "-")
-    print_beers(currentList)
+    print_beers(current_list)
     print(30 * "-" , "MENU" , 30 * "-")
     print("1. Generate Flight of Beer document.")
     print("2. Remove beer from current draft list.")
@@ -149,56 +162,57 @@ def print_menu():       ## Your menu design here
     print("4. Create new beer in archive.")
     print("5. Exit")
     print(66 * "-")
-    
-Running=True  
-while Running:          ## While loop which will keep going until loop = False
-    choice = None
-    while choice not in (1, 2, 3, 4, 5):
+
+RUNNING=True
+while RUNNING:          ## While loop which will keep going until loop = False
+    CHOICE = None
+    while CHOICE not in (1, 2, 3, 4, 5):
         print_menu()
         try:
-            choice = int(input("Enter your choice [1-5]: "))
+            CHOICE = int(input("Enter your choice [1-5]: "))
         except ValueError:
             print(f"{FAIL}That probably wasn't an option..{ENDC}")
             break
-        if(choice <= 0 or choice > 5 ):
+        if(CHOICE <= 0 or CHOICE > 5 ):
             print(f"{FAIL}Your options are [1-5]{ENDC}")
-    if choice==1:
-        beerList = parse_csv_file('currentdraft.txt')
-        
-        if beerList == None:
+    if CHOICE==1:
+        beer_list = parse_csv_file('currentdraft.txt')
+        if beer_list is None:
             continue
-        
-        print_beers(beerList)
-        chosenBeers = input("\nSelect your 4 beers pls! I.E. 1, 4, 5, 11\tChoose q to cancel!\n")
-        if(chosenBeers == 'q'):
-            continue
-        chosenBeers = chosenBeers.split(',')
 
-        chosenBeersList =[]
-        failed = False
-        for i in range(len(chosenBeers)):
-            try: 
-                index = int(chosenBeers[i])-1
+        print_beers(beer_list)
+        chosen_indices = input("\nSelect your 4 beers pls! I.E. 1, 4, 5, 11\tChoose q to cancel!\n")
+        if chosen_indices == 'q':
+            continue
+        chosen_indices = chosen_indices.split(',')
+
+        chosen_beers_list =[]
+        FAILED = False
+        for i, chosen_index in enumerate(chosen_indices):
+            try:
+                index_beer_list = int(chosen_index)-1
             except ValueError:
                 print(f'{FAIL}Sorry, did not understand that!{ENDC}')
-                failed = True
+                FAILED = True
                 break
-        
-            if(index < 0 or index >= len(beerList)):
+
+            if(index_beer_list < 0 or index_beer_list >= len(beer_list)):
                 print(F"{FAIL}Index out of range!{ENDC}")
-                failed = True
+                FAILED = True
                 break
-            chosenBeersList.append(beerList[index])
-        if(not failed):
-            Running = ~create_new_fob_doc(chosenBeersList)
-    elif choice==2:
+            chosen_beers_list.append(beer_list[index_beer_list])
+        if not FAILED:
+            RUNNING = ~create_new_fob_doc(chosen_beers_list)
+    elif CHOICE==2:
         remove_beer_from_current_draft_list()
-    elif choice==3:
+    elif CHOICE==3:
         add_beer_to_current_draft_list()
-    elif choice==4:
-        newBeer = get_new_beer_from_input()
-        if newBeer!= None:
-            print(f'\n{OKGREEN}Added {newBeer.name}{ENDC}\n' if save_new_beer_in_archive(newBeer, 'archive.txt') else F'\n{FAIL}Beer already in archive{ENDC}\n') 
-    elif choice==5:
+    elif CHOICE==4:
+        new_beer = get_new_beer_from_input()
+        if new_beer is not None:
+            print(f'\n{OKGREEN}Added {new_beer.name}{ENDC}\n'
+                  if save_new_beer_in_archive(new_beer, 'archive.txt')
+                  else F'\n{FAIL}Beer already in archive{ENDC}\n')
+    elif CHOICE==5:
         print ("OK BYE")
-        Running=False
+        RUNNING=False
